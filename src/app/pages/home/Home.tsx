6a0170ede6cc85e633f4ae471072a121c78360ab
KevinAlves55 <programador.kevin4773@gmail.com>
2022-11-21 18:10:39 -0300

    feat: Iniciando a estilização das informações do CEP

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -8,6 +8,33 @@ export const Home: React.FC = () => {
         <input type="text" />
         <button>Buscar</button>
       </div>
+
+      <div className={Style.container_infos_cep}>
+        <div>
+          <h2>Estado: </h2>
+          <span>SP</span>
+        </div>
+
+        <div>
+          <h2>Cidade: </h2>
+          <span>Itapevi</span>
+        </div>
+
+        <div>
+          <h2>Endereço: </h2>
+          <span>xxxxx xxxxxxx xxxxx xxx</span>
+        </div>
+
+        <div>
+          <h2>Rua: </h2>
+          <span>Maria José</span>
+        </div>
+
+        <div>
+          <h2>Código postal: </h2>
+          <span>456</span>
+        </div>
+      </div>
     </>
   );
 };
