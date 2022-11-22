8635a03a7827989c6680eb76b6dac40581ba5c49
KevinAlves55 <programador.kevin4773@gmail.com>
2022-11-22 18:39:02 -0300

    feat: Finalizado a listagem de informações do CEP

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { AppRoutes } from "./routes";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "./styles/Global.scss";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <ToastContainer autoClose={2000} />
     </BrowserRouter>
   );
 };
--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,8 +1,41 @@
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 import { PatternFormat } from "react-number-format";
 import { InfoCep } from "./components/InfoCep";
 import Style from "./Home.module.scss";
 
+interface IDadosCep {
+  cep: string,
+  logradouro: string,
+  complemento: string,
+  bairro: string,
+  localidade: string,
+  uf: string,
+  ibge: string,
+  gia: string,
+  ddd: string,
+  siafi: string
+}
+
 export const Home: React.FC = () => {
+  const [cep, setCep] = useState("");
+  const [dadosCep, setDadosCep] = useState<IDadosCep>({} as IDadosCep);
+
+  const buscarDados = async (cep: string) => {
+    if (cep === "") {
+      toast.error("CEP vazio");
+    } else {
+      axios(`https://viacep.com.br/ws/${cep}/json/`).then(result => {
+        if (!result.data.erro) {
+          setDadosCep(result.data);
+        } else {
+          toast.error("CEP não encontrado")
+        }
+      });
+    }
+  };
+
   return (
     <>
       <div className={Style.container}>
@@ -14,12 +47,19 @@ export const Home: React.FC = () => {
           className={Style.input}
           placeholder="Digite seu CEP..."
           type="text"
+          onChange={e => setCep(e.target.value)}
         />
-        <button>Buscar</button>
+        <button onClick={() => buscarDados(cep)}>Buscar</button>
       </div>
 
       <div className={Style.container_infos_cep}>
-        <InfoCep text="CEP: " value="06665-022" />
+        <InfoCep text="Cep: " value={dadosCep.cep ?? "Aguardando CEP..."} />
+        <InfoCep text="Estado: " value={dadosCep.uf ?? "Aguardando CEP..."} />
+        <InfoCep text="Cidade: " value={dadosCep.localidade ?? "Aguardando CEP..."} />
+        <InfoCep text="Endereço: " value={dadosCep.logradouro ?? "Aguardando CEP..."} />
+        <InfoCep text="Rua: " value={dadosCep.bairro ?? "Aguardando CEP..."} />
+        <InfoCep text="Código postal: " value={dadosCep.gia ?? "Aguardando CEP..."} />
+        <InfoCep text="DDD: " value={dadosCep.ddd ?? "Aguardando CEP..."} />
       </div>
     </>
   );
