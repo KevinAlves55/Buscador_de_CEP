7e161f626408a7cd90b97035b40ff85ed33ab0c0
KevinAlves55 <programador.kevin4773@gmail.com>
2022-11-23 16:29:42 -0300

    feat: Adicionando função de copiar para a área de transferência

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
+
 import { toast } from "react-toastify";
 import { PatternFormat } from "react-number-format";
+
 import { InfoCep } from "./components/InfoCep";
 import Style from "./Home.module.scss";
 
@@ -19,16 +21,16 @@ interface IDadosCep {
 }
 
 export const Home: React.FC = () => {
-  const [status, setStatus] = useState(false);
+  const [mostrarInformacoes, setMostrarInformacoes] = useState(false);
   const [cep, setCep] = useState("");
   const [dadosCep, setDadosCep] = useState<IDadosCep>({} as IDadosCep);
 
-  const buscarDados = async (cep: string) => {
+  const buscarDadosDoCep = async (cep: string) => {
     if (cep.length === 8) {
       axios(`https://viacep.com.br/ws/${cep}/json/`).then(result => {
         if (!result.data.erro) {
           setDadosCep(result.data);
-          setStatus(true);
+          setMostrarInformacoes(true);
         } else {
           toast.error("CEP não encontrado")
         }
@@ -50,19 +52,41 @@ export const Home: React.FC = () => {
           placeholder="Digite seu CEP..."
           type="text"
           onValueChange={e => setCep(e.value)}
+          autoFocus
         />
-        <button onClick={() => buscarDados(cep)}>Buscar</button>
+        <button onClick={() => buscarDadosDoCep(cep)}>Buscar</button>
       </div>
 
-      {(status && (
+      {(mostrarInformacoes && (
         <div className={Style.container_infos_cep}>
-          <InfoCep text="Cep: " value={dadosCep.cep === "" ? "Informação não encontrada" : dadosCep.cep} />
-          <InfoCep text="Estado: " value={dadosCep.uf === "" ? "Informação não encontrada" : dadosCep.uf} />
-          <InfoCep text="Cidade: " value={dadosCep.localidade === "" ? "Informação não encontrada" : dadosCep.localidade} />
-          <InfoCep text="Endereço: " value={dadosCep.logradouro === "" ? "Informação não encontrada" : dadosCep.logradouro} />
-          <InfoCep text="Rua: " value={dadosCep.bairro === "" ? "Informação não encontrada" : dadosCep.bairro} />
-          <InfoCep text="Código postal: " value={dadosCep.gia === "" ? "Informação não encontrada" : dadosCep.gia} />
-          <InfoCep text="DDD: " value={dadosCep.ddd === "" ? "Informação não encontrada" : dadosCep.ddd} />
+          <InfoCep
+            text="Cep: "
+            value={dadosCep.cep === "" ? "Informação não encontrada" : dadosCep.cep}
+          />
+          <InfoCep
+            text="Estado: "
+            value={dadosCep.uf === "" ? "Informação não encontrada" : dadosCep.uf}
+          />
+          <InfoCep
+            text="Cidade: "
+            value={dadosCep.localidade === "" ? "Informação não encontrada" : dadosCep.localidade}
+          />
+          <InfoCep
+            text="Endereço: "
+            value={dadosCep.logradouro === "" ? "Informação não encontrada" : dadosCep.logradouro}
+          />
+          <InfoCep
+            text="Rua: "
+            value={dadosCep.bairro === "" ? "Informação não encontrada" : dadosCep.bairro}
+          />
+          <InfoCep
+            text="Código postal: "
+            value={dadosCep.gia === "" ? "Informação não encontrada" : dadosCep.gia}
+          />
+          <InfoCep
+            text="DDD: "
+            value={dadosCep.ddd === "" ? "Informação não encontrada" : dadosCep.ddd}
+          />
         </div>
       ))}
     </>
--- src/app/pages/home/components/InfoCep.tsx
+++ src/app/pages/home/components/InfoCep.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
+import copy from "copy-to-clipboard";
+import { BsClipboard } from "react-icons/bs";
+
+import Styles from "./InfoCep.module.scss";
+import { toast } from "react-toastify";
+
 interface IInfoCep {
   text: string;
   value: string | number;
 }
 
 export const InfoCep: React.FC<IInfoCep> = ({ text, value }) => {
+  const copiarParaAreaDeTransferencia = () => {
+    copy(String(value));
+    toast.success("Dado copiado para a área de transferência")
+  };
+
   return (
     <div>
       <h3>{text}</h3>
       <span>{value}</span>
+      <BsClipboard
+        className={Styles.clipboard}
+        title="Copiar texto"
+        onClick={copiarParaAreaDeTransferencia}
+      />
     </div>
   );
 };
